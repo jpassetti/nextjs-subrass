8316b7f814375e9cfcc17b1966b1feab36b9be3d
Jeff Passetti <jpassetti@gmail.com>
2022-07-24 14:34:44 -0400

    about and contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,12 @@
 import Layout from '../components/layout'
 import Heading from '../components/heading'
+import Section from '../components/section';
 
 const Contact = () => {
 	return <Layout>
+		<Section>
 		<Heading level="1">Contact</Heading>
+		</Section>
 	</Layout>
 }
 export default Contact;
