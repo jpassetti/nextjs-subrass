1b89ae9bf0946ed8b902d42667664c8e7d761de3
Jeff Passetti <jpassetti@gmail.com>
2022-08-16 17:07:35 -0400

    contact page

--- components/paragraph.js
+++ components/paragraph.js
@@ -3,11 +3,17 @@ import * as styles from './paragraph.module.scss';
 
 let cx = classNames.bind(styles);
 
-const Paragraph = ({type, children, className}) => {
+const Paragraph = ({
+	type, 
+	children, 
+	className,
+	marginBottom, 
+}) => {
 
 	let paragraphClasses = cx({
 		paragraph: true,
-		intro: type === 'intro'
+		intro: type === 'intro',
+		[`margin-bottom-${marginBottom}`] : marginBottom
 	});
 	return <p className={`${paragraphClasses} ${className}`}>{children}</p>
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,20 @@
 import Layout from '../components/layout'
 import Heading from '../components/heading'
+import Label from '../components/Label';
 import Section from '../components/section';
+import Paragraph from '../components/paragraph';
 
 const Contact = () => {
 	return <Layout>
 		<Section>
 			<Heading level="1" marginTop="8" marginBottom="4">Contact</Heading>
+			<Paragraph type="intro" marginBottom="4">Call us today at (315)443-3436 or email us at <a href="mailto:subrass@syr.edu" target="_blank">subrass@syr.edu</a>.</Paragraph>
+			<Paragraph>
+				<Label>Mailing address</Label>
+				Room 1-014, Center for Science and Technology<br />
+				Syracuse University<br />
+				Syracuse, NY 13244-4100
+			</Paragraph>
 		</Section>
 	</Layout>
 }
