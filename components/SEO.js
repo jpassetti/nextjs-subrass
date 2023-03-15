0c4b109cf533dd0c207ad84ce37b93556aefc10b
Jeff Passetti <jpassetti@Jeffs-MacBook-Air.local>
2023-03-15 18:05:33 -0400

    refactored SEO component

--- components/SEO.js
+++ components/SEO.js
@@ -2,21 +2,21 @@ import Head from 'next/head'
 import { Fragment } from 'react'
 
 const DOMAIN = 'https://subrass.syr.edu';
+const SITENAME = "Syracuse University Brass Ensemble";
 
 const SEO = ({
     title=false,
     description="The Syracuse University Brass Ensemble (SUBE) is a group of 35 professional-level brass and percussion musicians.",
     image=false,
     url=DOMAIN,
-    siteName=false
 }) => {
     return <Head>
-    <title>{`${title ? `${title} | ` : ''}Syracuse University Brass Ensemble`}</title>
-    <meta key="og_title" property="og:title" content={`${title ? `${title} | ` : ''}Syracuse University Brass Ensemble`} />
-    <meta key="twitter_title" property="twitter:title" content={`${title ? `${title} | ` : ''}Syracuse University Brass Ensemble`} />
+    <title>{`${title ? `${title} | ` : ''}${SITENAME}`}</title>
+    <meta key="og_title" property="og:title" content={`${title ? `${title} | ` : ''}${SITENAME}`} />
+    <meta key="twitter_title" property="twitter:title" content={`${title ? `${title} | ` : ''}${SITENAME}`} />
     <meta key="viewport" name="viewport" content="initial-scale=1.0, width=device-width" />
     <meta key="charset" charSet="UTF-8" />
-    <meta key="og_site_name" property="og:site_name" content="Syracuse University Brass Ensemble" />
+    <meta key="og_site_name" property="og:site_name" content={SITENAME} />
     <meta key="og_country_name" property="og:country_name" content="USA" />
     <meta key="og_locale" property="og:locale" content="en_US" />
     <meta key="og_type" property="og:type" content="website" />
@@ -36,13 +36,11 @@ const SEO = ({
 	<link key="icon_96" rel="icon" type="image/png" sizes="96x96" href="/images/favicons/favicon-96.png" />
 	<link key="icon_144" rel="icon" type="image/png" sizes="144x144" href="/images/favicons/favicon-144.png" />
 	<link key="icon_192" rel="icon" type="image/png" sizes="192x192" href="/images/favicons/favicon-192.png" />
-    {description &&
-        <Fragment>
-            <meta key="description" name="description" content={description} />
-            <meta key="og_description" property="og:description" content={description} />
-            <meta key="twitter_description" property="twitter:description" content={description} />
-        </Fragment>
-    }
+    
+    <meta key="description" name="description" content={description} />
+    <meta key="og_description" property="og:description" content={description} />
+    <meta key="twitter_description" property="twitter:description" content={description} />
+     
     {image &&
         <Fragment>
             <meta
@@ -57,13 +55,11 @@ const SEO = ({
             <meta key="twitter_image_alt" property="twitter:image:alt" content={image.alt} />
         </Fragment>
     }
-    {url &&
-        <Fragment>
-            <meta key="url" name="url" content={url ? url : DOMAIN} />
-            <meta key="og_url" property="og:url" content={url ? url : DOMAIN} />
-            <meta key="twitter_url" property="twitter:url" content={url ? url : DOMAIN} />
-        </Fragment>
-    }
+   
+    <meta key="url" name="url" content={url} />
+    <meta key="og_url" property="og:url" content={url} />
+    <meta key="twitter_url" property="twitter:url" content={url} />
+    
 </Head>
 }
 export default SEO; 
