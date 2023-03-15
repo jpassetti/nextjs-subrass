99016f04c339ead45bbc884296336b1b489e03cd
Jeff Passetti <jpassetti@Jeffs-MacBook-Air.local>
2023-03-15 18:02:02 -0400

    improved SEO component

--- components/SEO.js
+++ components/SEO.js
@@ -13,22 +13,22 @@ const SEO = ({
     return <Head>
     <title>{`${title ? `${title} | ` : ''}Syracuse University Brass Ensemble`}</title>
     <meta key="og_title" property="og:title" content={`${title ? `${title} | ` : ''}Syracuse University Brass Ensemble`} />
-    <meta key="twitter_title" name="twitter:title" content={`${title ? `${title} | ` : ''}Syracuse University Brass Ensemble`} />
+    <meta key="twitter_title" property="twitter:title" content={`${title ? `${title} | ` : ''}Syracuse University Brass Ensemble`} />
     <meta key="viewport" name="viewport" content="initial-scale=1.0, width=device-width" />
     <meta key="charset" charSet="UTF-8" />
     <meta key="og_site_name" property="og:site_name" content="Syracuse University Brass Ensemble" />
     <meta key="og_country_name" property="og:country_name" content="USA" />
     <meta key="og_locale" property="og:locale" content="en_US" />
     <meta key="og_type" property="og:type" content="website" />
-    <meta key="og_locality" name='og:locality' content='Syracuse' />
-    <meta key="og_region" name='og:region' content='NY' />
-    <meta key="og_postal_code" name='og:postal-code' content='13244' />
+    <meta key="og_locality" property='og:locality' content='Syracuse' />
+    <meta key="og_region" property='og:region' content='NY' />
+    <meta key="og_postal_code" property='og:postal-code' content='13244' />
     <meta key="robots" name='robots' content='index,follow' />
     <meta key="language" name="language" content="EN" />
 	<meta key="copyright" name="copyright" content="Syracuse University" />
     <meta key="google_site_verification" name="google-site-verification" content="axCJkiNnnco1_eFCM3DMVXx2-PZEGmZ9ldfmq1E6dVM" />
-    <meta key="twitter_card" name="twitter:card" content="summary" />
-    <meta key="twitter_site" name="twitter:site" content="@thesubrass" />	
+    <meta key="twitter_card" property="twitter:card" content="summary" />
+    <meta key="twitter_site" property="twitter:site" content="@thesubrass" />	
     <meta key="keywords" name="keywords" content="Syracuse, Syracuse University, Syracuse University Brass, Syracuse University Brass Ensemble, Hendricks Chapel, SU Brass, SUBE, CNY Brass, Brass, Classical, Music, James T. Spencer, New York, Concerts" />
     <link key="icon_apple_touch" rel="apple-touch-icon" href="/images/favicons/apple-touch-icon.png" />
 	<link key="icon_16" rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16.png" />
@@ -40,7 +40,7 @@ const SEO = ({
         <Fragment>
             <meta key="description" name="description" content={description} />
             <meta key="og_description" property="og:description" content={description} />
-            <meta key="twitter_description" name="twitter_description" content={description} />
+            <meta key="twitter_description" property="twitter:description" content={description} />
         </Fragment>
     }
     {image &&
@@ -52,15 +52,16 @@ const SEO = ({
             />
             <meta key="og_image_width" property="og:image:width" content={image.width} />
             <meta key="og_image_height" property="og:image:height" content={image.height} />
-            <meta key="og_image_alt" name="og:image:alt" content={image.alt} />
-            <meta key="twitter_image_alt" name="twitter:image:alt" content={image.alt} />
+            <meta key="og_image_alt" property="og:image:alt" content={image.alt} />
+            <meta key="twitter_image" name="twitter:image" content={image.src} />
+            <meta key="twitter_image_alt" property="twitter:image:alt" content={image.alt} />
         </Fragment>
     }
     {url &&
         <Fragment>
             <meta key="url" name="url" content={url ? url : DOMAIN} />
-            <meta key="og_url" name="og:url" content={url ? url : DOMAIN} />
-            <meta key="twitter_url" name="twitter:url" content={url ? url : DOMAIN} />
+            <meta key="og_url" property="og:url" content={url ? url : DOMAIN} />
+            <meta key="twitter_url" property="twitter:url" content={url ? url : DOMAIN} />
         </Fragment>
     }
 </Head>
