dc362f88548bb6f99498026726cae56f8ed994cd
Jeff Passetti <jpassetti@gmail.com>
2023-09-13 13:04:10 -0400

    google analytics 4

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/global.scss'
 import { ScrollContext, ModalContext } from '../lib/context'
 import { useState, useEffect } from 'react'
+import Script from 'next/script'
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 export default function App({ Component, pageProps }) {
 	const [isModalOpen, setModalOpen] = useState(false);
@@ -23,9 +26,43 @@ export default function App({ Component, pageProps }) {
 			window.scrollTo(0, scrollPosition);
 		}
 	}, [isModalOpen]);
-	return <ScrollContext.Provider value={[scrollYObj, setScrollYObj]}>
+
+	const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+
+	return <>
+	{/* Global Site Tag (gtag.js) - Google Analytics */}
+	<Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
+	<ScrollContext.Provider value={[scrollYObj, setScrollYObj]}>
 		<ModalContext.Provider value={[isModalOpen, setModalOpen]}>
 			<Component {...pageProps} />
 		</ModalContext.Provider>
 	</ScrollContext.Provider>
+	</>
 }
