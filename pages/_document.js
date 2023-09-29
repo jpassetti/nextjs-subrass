29993f2150c6a9dd9d436d21801e20d31b09ca53
Jeff Passetti <jpassetti@gmail.com>
2023-09-29 18:01:16 -0400

    google search console verification

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ class MyDocument extends Document {
 					<link rel="stylesheet" href="https://use.typekit.net/nnm0mtl.css" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet" />
+					<meta name="google-site-verification" content="l26PfsrOTavwnJHFn2NCyqFYO7CMdnIPQs3SVPkiJ3o" />
 				</Head>
 				<body>
 					<Main />
