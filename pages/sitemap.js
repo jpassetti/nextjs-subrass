d1b881b1fcefbd44e1da9dfeb4245d251ced25fe
Jeff Passetti <jpassetti@gmail.com>
2023-10-17 14:42:49 -0400

    simplified date format

--- pages/sitemap.js
+++ pages/sitemap.js
@@ -8,10 +8,12 @@ function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${posts.map(({ path, lastmod, changefreq }) => {
+        // Extract the date part (YYYY-MM-DD) from lastmod
+        const datePart = lastmod ? lastmod.split('T')[0] : '';
         return `
         <url>
             <loc>${`${EXTERNAL_DATA_URL}${path}`}</loc>
-            ${lastmod ? `<lastmod>${lastmod}</lastmod>` : ''}
+            ${lastmod ? `<lastmod>${datePart}</lastmod>` : ''}
             ${changefreq ? `<changefreq>${changefreq}</changefreq>` : ''}
         </url>
         `;
