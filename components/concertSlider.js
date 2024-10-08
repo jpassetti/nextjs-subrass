45c6bf6f5d8fdb26268d9689962627dc66997707
Jeff Passetti <jpassetti@gmail.com>
2024-10-08 11:06:11 -0400

    slidesPerView updated to 1.25

--- components/concertSlider.js
+++ components/concertSlider.js
@@ -1,6 +1,5 @@
-
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 // import required modules
 import { Pagination } from "swiper";
 
@@ -9,46 +8,48 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import Card from '../components/card'
-import Concert from '../components/concert'
+import Card from "../components/card";
+import Concert from "../components/concert";
 
+import styles from "./concertslider.module.scss";
 
-import styles from './concertslider.module.scss'
-
-
-const ConcertSlider = ({concerts}) => {
-	return <Swiper
-			slidesPerView={1.5}
-			spaceBetween={16}
-			navigation={false}
-			pagination={{
-				clickable: true,
-				bulletActiveClass: styles.bulletActive
-			}}
-			breakpoints={{
-				700: {
-					slidesPerView: 2,
-					//spaceBetween: 20,
-				},
-				980: {
-					slidesPerView: 3,
-					// spaceBetween: 40,
-				},
-				1140: {
-					slidesPerView: 3,
-					// spaceBetween: 50,
-				},
-			}}
-			modules={[Pagination]}
-			className={styles.mySwiper}
-		>
-			{concerts.map((concert, index) => {
-				return <SwiperSlide key={index}>
-					<Card>
-						<Concert data={concert.node} teaser />
-					</Card>
-				</SwiperSlide>
-			})}
-	</Swiper>
-}
+const ConcertSlider = ({ concerts }) => {
+ return (
+  <Swiper
+   slidesPerView={1.25}
+   spaceBetween={16}
+   navigation={false}
+   pagination={{
+    clickable: true,
+    bulletActiveClass: styles.bulletActive,
+   }}
+   breakpoints={{
+    700: {
+     slidesPerView: 2,
+     //spaceBetween: 20,
+    },
+    980: {
+     slidesPerView: 3,
+     // spaceBetween: 40,
+    },
+    1140: {
+     slidesPerView: 3,
+     // spaceBetween: 50,
+    },
+   }}
+   modules={[Pagination]}
+   className={styles.mySwiper}
+  >
+   {concerts.map((concert, index) => {
+    return (
+     <SwiperSlide key={index}>
+      <Card>
+       <Concert data={concert.node} teaser />
+      </Card>
+     </SwiperSlide>
+    );
+   })}
+  </Swiper>
+ );
+};
 export default ConcertSlider;
