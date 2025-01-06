01c9409a69786e368b02e2932c1b1f025f2f6fa0
Jeff Passetti <jpassetti@gmail.com>
2025-01-06 16:11:21 -0500

    2024-25 Ensemble

--- components/musician.js
+++ components/musician.js
@@ -1,188 +1,273 @@
-import { Fragment } from 'react'
-import styles from './musician.module.scss';
+import { Fragment } from "react";
+import styles from "./musician.module.scss";
 
-import Col from './col'
-import Link from 'next/link'
-import Head from 'next/head';
-import Heading from './heading';
-import MainContent from './maincontent'
-import Paragraph from './paragraph';
-import ProfileImage from './profileimage';
-import Row from './row'
-import SEO from './SEO'
-import Span from './span'
+import Col from "./col";
+import Link from "next/link";
+import Head from "next/head";
+import Heading from "./heading";
+import MainContent from "./maincontent";
+import Paragraph from "./paragraph";
+import ProfileImage from "./profileimage";
+import Row from "./row";
+import SEO from "./SEO";
+import Span from "./span";
 
+import {
+ isGraduateOfSyracuseUniversity,
+ truncateGraduationYear,
+ doesEducationIncludeSyracuseUniversity,
+ doesWorkIncludeSyracuseUniversity,
+ displaySyracuseUniversityJobTitles,
+} from "../lib/utilities";
 
-import { isGraduateOfSyracuseUniversity, truncateGraduationYear, doesEducationIncludeSyracuseUniversity, doesWorkIncludeSyracuseUniversity, displaySyracuseUniversityJobTitles } from '../lib/utilities'
+const Musician = ({ data, teaser = false }) => {
+ const { slug, content, instruments, featuredImage, peopleTypes } = data;
+ const { prefix, firstName, middleInitial, lastName, suffix, education, work } =
+  data.personInformation;
 
-const Musician = ({data, teaser=false}) => {
-	const { slug, content, instruments, featuredImage, peopleTypes } = data;
-	const { prefix, firstName, middleInitial, lastName, suffix, education, work } = data.personInformation;
+ const buildFullName = () => {
+  let nameParts = [];
+  if (prefix) nameParts.push(prefix);
+  nameParts.push(firstName);
+  if (middleInitial) nameParts.push(`${middleInitial}.`);
+  nameParts.push(lastName);
+  if (suffix) nameParts.push(`, ${suffix}`);
 
-	const buildFullName = () => {
-		let nameParts = [];
-		if (prefix) nameParts.push(prefix);
-		nameParts.push(firstName);
-		if (middleInitial) nameParts.push(`${middleInitial}.`);
-		nameParts.push(lastName);
-		if (suffix) nameParts.push(`, ${suffix}`);
-		
-		return nameParts.join(' ');
-	}
+  return nameParts.join(" ");
+ };
 
-	function addProductJsonLd() {
-		
+ function addProductJsonLd() {
+  const schema = {
+   "@context": "https://schema.org",
+   "@type": "Person",
+   name: buildFullName(),
+   description: "A member of the Syracuse University Brass Ensemble",
+   image: featuredImage ? featuredImage.node.sourceUrl : "",
+   jobTitle:
+    slug === "james-t-spencer"
+     ? "Music Director"
+     : instruments.edges[0].node.name,
+   affiliation: "Syracuse University Brass Ensemble",
+  };
 
-		const schema = {
-			"@context": "https://schema.org",
-			"@type": "Person",
-			"name": buildFullName(),
-			"description": "A member of the Syracuse University Brass Ensemble",
-			"image": featuredImage ? featuredImage.node.sourceUrl : '',
-			"jobTitle": slug === "james-t-spencer" ? "Music Director" : instruments.edges[0].node.name,
-			"affiliation": "Syracuse University Brass Ensemble"
-		  };
-	
-		return {
-			__html: JSON.stringify(schema)
-		};
-	}
+  return {
+   __html: JSON.stringify(schema),
+  };
+ }
 
-	const displayInstruments = (instruments) => {
-		const instrumentsArr = instruments.edges.map((instrument) => {
-			return instrument.node.name;
-		})
-		//console.log({instrumentsArr});
-		const instrumentsString = instrumentsArr.join(', ');
-		return instrumentsString;
-	}
+ const displayInstruments = (instruments) => {
+  const instrumentsArr = instruments.edges.map((instrument) => {
+   return instrument.node.name;
+  });
+  //console.log({instrumentsArr});
+  const instrumentsString = instrumentsArr.join(", ");
+  return instrumentsString;
+ };
 
-	const fullName = (teaser) => {
-		return teaser ?
-			<Fragment>
-				<Span fontWeight="normal" fontSize="smaller">{prefix ? prefix : ''} {firstName} {middleInitial ? `${middleInitial}.` : ''}</Span><br />
-				<Span fontWeight="bold" textTransform="uppercase">{lastName}{suffix ? `, ${suffix}` : ''} {education?.map((item, index) => {
-					return isGraduateOfSyracuseUniversity(item.university) ? `${truncateGraduationYear(item.graduationYear, item.degreeType)} ` : ''
-				})}</Span>
-			</Fragment>
-		: 
-		<Fragment>
-			<Span fontWeight="400" fontColor="white" fontSize="smaller">{prefix ? prefix : ''} {firstName} {middleInitial ? `${middleInitial}.` : ''} </Span><br />
-			<Span fontWeight="bold" textTransform="uppercase">{lastName}{suffix ? `, ${suffix}` : ''} {education?.map((item, index) => {
-				return isGraduateOfSyracuseUniversity(item.university) ? `${truncateGraduationYear(item.graduationYear, item.degreeType)} ` : ''
-			})}</Span>
-		</Fragment>		
-	}
-	
-	return teaser ? <Row alignItems="center" marginBottom="2">
-		<Col xs="4" sm="3" md="4" marginBottom="0">
-			<Link href={`/about/musicians/${slug}`}>
-				<a>
-					<ProfileImage
-						featuredImage={featuredImage}
-						isSUGraduate={education ? doesEducationIncludeSyracuseUniversity(education) : false}
-						isSUProfessor={work ? doesWorkIncludeSyracuseUniversity(work) : false}
-					/>
-				</a>
-			</Link>
-		</Col>
-		<Col xs="8" sm="9" md="8" marginBottom="0">
-			<Heading level="3">
-				<Link href={`/about/musicians/${slug}`}>
-					<a>
-				<Span fontWeight="normal" fontSize="smaller">{prefix ? prefix : ''} {firstName} {middleInitial ? `${middleInitial}.` : ''} </Span><br />
-				<Span fontWeight="bold" textTransform="uppercase">{lastName}{suffix ? `, ${suffix}` : ''} {education?.map((item, index) => {
-					return isGraduateOfSyracuseUniversity(item.university) ? `${truncateGraduationYear(item.graduationYear, item.degreeType)} ` : ''
-				})}</Span>
-				</a></Link>
-			</Heading>
-			{work && doesWorkIncludeSyracuseUniversity(work) ? 
-				<Heading level="4" fontWeight="500" marginTop="1" fontFamily="secondary">
-				{displaySyracuseUniversityJobTitles(work)}
-				</Heading>
-			: ''}
-			
-		</Col>
-	</Row>
+ const fullName = (teaser) => {
+  return teaser ? (
+   <Fragment>
+    <Span fontWeight="normal" fontSize="smaller">
+     {prefix ? prefix : ""} {firstName}{" "}
+     {middleInitial ? `${middleInitial}.` : ""}
+    </Span>
+    <br />
+    <Span fontWeight="bold" textTransform="uppercase">
+     {lastName}
+     {suffix ? `, ${suffix}` : ""}{" "}
+     {education?.map((item, index) => {
+      return isGraduateOfSyracuseUniversity(item.university)
+       ? `${truncateGraduationYear(item.graduationYear, item.degreeType)} `
+       : "";
+     })}
+    </Span>
+   </Fragment>
+  ) : (
+   <Fragment>
+    <Span fontWeight="400" fontColor="white" fontSize="smaller">
+     {prefix ? prefix : ""} {firstName}{" "}
+     {middleInitial ? `${middleInitial}.` : ""}{" "}
+    </Span>
+    <br />
+    <Span fontWeight="bold" textTransform="uppercase">
+     {lastName}
+     {suffix ? `, ${suffix}` : ""}{" "}
+     {education?.map((item, index) => {
+      return isGraduateOfSyracuseUniversity(item.university)
+       ? `${truncateGraduationYear(item.graduationYear, item.degreeType)} `
+       : "";
+     })}
+    </Span>
+   </Fragment>
+  );
+ };
 
-	:
-	<Fragment>
-		<SEO 
-			title={`${buildFullName()} - ${slug === "james-t-spencer" ? "Music Director" : instruments.edges[0].node.name} - Syracuse University Brass Ensemble`}
-			url={`https://subrass.syr.edu/about/musicians/${slug}`}
-			/>
-		<Head>
-			<script
-				type="application/ld+json"
-				dangerouslySetInnerHTML={addProductJsonLd()}
-				key="product-jsonld"
-			/>
-		</Head>
-		<div className={styles.showcase}>
-			<Heading level="4" marginBottom="4" color="white"><Link href="/ensembles/2023-24">
-				<a>
-					&laquo; Musicians
-				</a></Link></Heading>
-			<Row alignItems="flex-start">
-				<Col xs="6" sm="6" md="2">
-					<ProfileImage 
-					featuredImage={featuredImage} 
-					isSUGraduate={education ? doesEducationIncludeSyracuseUniversity(education) : false} 
-					isSUProfessor={work ? doesWorkIncludeSyracuseUniversity(work) : false} 
-					/>
-				</Col>
-				<Col xs="12" sm="12" md="8">
-					<Heading level="1" marginBottom="2" color="white" lineHeight="normal">{fullName()}</Heading>
-					
-					{work && work.map((item, index) => {
-						const { jobTitle, companySubdivision, companySubdivisionUrl, companyName, companyUrl } = item;
-						return <Heading key={index} level="3" marginBottom="4" color="white" fontWeight="500" lineHeight="normal">
-							{jobTitle &&
-								<Span display="block" marginBottom="1" fontFamily="secondary" fontWeight="500">
-									{jobTitle}
-								</Span>
-							}
-							{companySubdivision &&
-								<Span display="block" marginBottom="1">
-									{companySubdivisionUrl ?
-										<a href={companySubdivisionUrl} target="_blank">
-												{companySubdivision}
-										</a>
-									: 
-										companySubdivision
-									}	
-									</Span>
-							}
-							{companyName && 
-								<Span display="block" fontSize="smaller" textTransform="uppercase" letterSpacing="spaced">
-									{companyUrl ?
-										<a href={companyUrl} target="_blank">
-											{companyName}
-										</a>
-										:
-										companyName
-									}	
-								</Span>
-							}
-							</Heading>
-					})}
-					{instruments &&
-						<Fragment>
-							<Heading level="4" color="orange" textTransform="uppercase" marginBottom="1" marginTop="4">Instrument{instruments.edges.length > 1 ? 's' : ''}</Heading>
-							<Heading level="3" marginBottom="2" color="white" fontWeight="400">{displayInstruments(instruments)}
-							</Heading>
-						</Fragment>
-					}
-			</Col>
-			</Row>
-		</div>
-		<Row justifyContent="center">
-			<Col xs="12" sm="8" paddingTop="8">
-				{content &&
-					<MainContent content={content} />
-				}
-			</Col>
-		</Row>	
-	</Fragment>
-}
+ return teaser ? (
+  <Row alignItems="center" marginBottom="2">
+   <Col xs="4" sm="3" md="4" marginBottom="0">
+    <Link href={`/about/musicians/${slug}`}>
+     <a>
+      <ProfileImage
+       featuredImage={featuredImage}
+       isSUGraduate={
+        education ? doesEducationIncludeSyracuseUniversity(education) : false
+       }
+       isSUProfessor={work ? doesWorkIncludeSyracuseUniversity(work) : false}
+      />
+     </a>
+    </Link>
+   </Col>
+   <Col xs="8" sm="9" md="8" marginBottom="0">
+    <Heading level="3">
+     <Link href={`/about/musicians/${slug}`}>
+      <a>
+       <Span fontWeight="normal" fontSize="smaller">
+        {prefix ? prefix : ""} {firstName}{" "}
+        {middleInitial ? `${middleInitial}.` : ""}{" "}
+       </Span>
+       <br />
+       <Span fontWeight="bold" textTransform="uppercase">
+        {lastName}
+        {suffix ? `, ${suffix}` : ""}{" "}
+        {education?.map((item, index) => {
+         return isGraduateOfSyracuseUniversity(item.university)
+          ? `${truncateGraduationYear(item.graduationYear, item.degreeType)} `
+          : "";
+        })}
+       </Span>
+      </a>
+     </Link>
+    </Heading>
+    {work && doesWorkIncludeSyracuseUniversity(work) ? (
+     <Heading level="4" fontWeight="500" marginTop="1" fontFamily="secondary">
+      {displaySyracuseUniversityJobTitles(work)}
+     </Heading>
+    ) : (
+     ""
+    )}
+   </Col>
+  </Row>
+ ) : (
+  <Fragment>
+   <SEO
+    title={`${buildFullName()} - ${
+     slug === "james-t-spencer"
+      ? "Music Director"
+      : instruments.edges[0].node.name
+    } - Syracuse University Brass Ensemble`}
+    url={`https://subrass.syr.edu/about/musicians/${slug}`}
+   />
+   <Head>
+    <script
+     type="application/ld+json"
+     dangerouslySetInnerHTML={addProductJsonLd()}
+     key="product-jsonld"
+    />
+   </Head>
+   <div className={styles.showcase}>
+    <Heading level="4" marginBottom="4" color="white">
+     <Link href="/ensembles/2024-25">
+      <a>&laquo; Musicians</a>
+     </Link>
+    </Heading>
+    <Row alignItems="flex-start">
+     <Col xs="6" sm="6" md="2">
+      <ProfileImage
+       featuredImage={featuredImage}
+       isSUGraduate={
+        education ? doesEducationIncludeSyracuseUniversity(education) : false
+       }
+       isSUProfessor={work ? doesWorkIncludeSyracuseUniversity(work) : false}
+      />
+     </Col>
+     <Col xs="12" sm="12" md="8">
+      <Heading level="1" marginBottom="2" color="white" lineHeight="normal">
+       {fullName()}
+      </Heading>
+
+      {work &&
+       work.map((item, index) => {
+        const {
+         jobTitle,
+         companySubdivision,
+         companySubdivisionUrl,
+         companyName,
+         companyUrl,
+        } = item;
+        return (
+         <Heading
+          key={index}
+          level="3"
+          marginBottom="4"
+          color="white"
+          fontWeight="500"
+          lineHeight="normal"
+         >
+          {jobTitle && (
+           <Span
+            display="block"
+            marginBottom="1"
+            fontFamily="secondary"
+            fontWeight="500"
+           >
+            {jobTitle}
+           </Span>
+          )}
+          {companySubdivision && (
+           <Span display="block" marginBottom="1">
+            {companySubdivisionUrl ? (
+             <a href={companySubdivisionUrl} target="_blank">
+              {companySubdivision}
+             </a>
+            ) : (
+             companySubdivision
+            )}
+           </Span>
+          )}
+          {companyName && (
+           <Span
+            display="block"
+            fontSize="smaller"
+            textTransform="uppercase"
+            letterSpacing="spaced"
+           >
+            {companyUrl ? (
+             <a href={companyUrl} target="_blank">
+              {companyName}
+             </a>
+            ) : (
+             companyName
+            )}
+           </Span>
+          )}
+         </Heading>
+        );
+       })}
+      {instruments && (
+       <Fragment>
+        <Heading
+         level="4"
+         color="orange"
+         textTransform="uppercase"
+         marginBottom="1"
+         marginTop="4"
+        >
+         Instrument{instruments.edges.length > 1 ? "s" : ""}
+        </Heading>
+        <Heading level="3" marginBottom="2" color="white" fontWeight="400">
+         {displayInstruments(instruments)}
+        </Heading>
+       </Fragment>
+      )}
+     </Col>
+    </Row>
+   </div>
+   <Row justifyContent="center">
+    <Col xs="12" sm="8" paddingTop="8">
+     {content && <MainContent content={content} />}
+    </Col>
+   </Row>
+  </Fragment>
+ );
+};
 export default Musician;
--- components/nav.js
+++ components/nav.js
@@ -1,121 +1,126 @@
-import { useState } from 'react';
-import Link from 'next/link'
-import { useRouter } from 'next/router';
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-import classNames from 'classnames/bind';
-import Paragraph from './paragraph';
-import PasswordModal from './passwordmodal';
-import * as styles from './nav.module.scss';
+import classNames from "classnames/bind";
+import Paragraph from "./paragraph";
+import PasswordModal from "./passwordmodal";
+import * as styles from "./nav.module.scss";
 
 let cx = classNames.bind(styles);
 
 const navLinks = [
-	{
-		title: "Home",
-		path: "/"
-	},
-	{
-		title: "About",
-		path: "/about"
-	},
-	{
-		title: "Concerts",
-		path: "/concerts"
-	},
-	{
-		title: "Musicians",
-		path: "/ensembles/2023-24"
-	},
-	{
-		title: "Contact",
-		path: "/contact"
-	},
+ {
+  title: "Home",
+  path: "/",
+ },
+ {
+  title: "About",
+  path: "/about",
+ },
+ {
+  title: "Concerts",
+  path: "/concerts",
+ },
+ {
+  title: "Musicians",
+  path: "/ensembles/2024-25",
+ },
+ {
+  title: "Contact",
+  path: "/contact",
+ },
 ];
 
-
 const Nav = () => {
-	const router = useRouter();
-	let navClasses = cx({
-		nav: true
-	});
-	return (
-		<nav className={navClasses}>
-			<ul>
-				{navLinks.map((navLink, index) => {
-					const {path, title} = navLink;
-					return <li key={index}>
-						<Link href={path}>
-							<a className={router.pathname == path ? styles.active : ""}>
-								{title}
-							</a>
-						</Link>
-					</li>
-				})}
-			</ul>
-		</nav>
-	)
-}
+ const router = useRouter();
+ let navClasses = cx({
+  nav: true,
+ });
+ return (
+  <nav className={navClasses}>
+   <ul>
+    {navLinks.map((navLink, index) => {
+     const { path, title } = navLink;
+     return (
+      <li key={index}>
+       <Link href={path}>
+        <a className={router.pathname == path ? styles.active : ""}>{title}</a>
+       </Link>
+      </li>
+     );
+    })}
+   </ul>
+  </nav>
+ );
+};
 const SocialNav = () => {
-	let socialNavClasses = cx({
-		socialnav: true
-	});
-	return <nav className={socialNavClasses}><ul>
-		<li>
-			<a href="http://facebook.com/subrass" target="_blank">Facebook</a>
-		</li>
-		<li>
-			<a href="http://soundcloud.com/thesubrass" target="_blank">Soundcloud</a>
-		</li>
-		<li>
-			<a href="http://youtube.com/thesubrass" target="_blank">YouTube</a>
-		</li>
-		</ul>
-	</nav>
-}
+ let socialNavClasses = cx({
+  socialnav: true,
+ });
+ return (
+  <nav className={socialNavClasses}>
+   <ul>
+    <li>
+     <a href="http://facebook.com/subrass" target="_blank">
+      Facebook
+     </a>
+    </li>
+    <li>
+     <a href="http://soundcloud.com/thesubrass" target="_blank">
+      Soundcloud
+     </a>
+    </li>
+    <li>
+     <a href="http://youtube.com/thesubrass" target="_blank">
+      YouTube
+     </a>
+    </li>
+   </ul>
+  </nav>
+ );
+};
 Nav.SocialNav = SocialNav;
 const Members = () => {
-	const [isModalOpen, setIsModalOpen] = useState(false);
+ const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleLinkClick = () => {
-    setIsModalOpen(true);
-  };
-  	
+ const handleLinkClick = () => {
+  setIsModalOpen(true);
+ };
 
-
-	return <nav className={styles.socialnav}>
-		<ul>
-			<li onClick={handleLinkClick}>
-				Members login
-			</li>
-		</ul>
-		{isModalOpen && (
-        <PasswordModal
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-        />
-      )}
-	</nav>
-}
+ return (
+  <nav className={styles.socialnav}>
+   <ul>
+    <li onClick={handleLinkClick}>Members login</li>
+   </ul>
+   {isModalOpen && (
+    <PasswordModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+   )}
+  </nav>
+ );
+};
 Nav.Members = Members;
 const Mobile = () => {
-	const router = useRouter();
-	let mobileNavClasses = cx({
-		mobileNav: true
-	});
-	return <nav className={mobileNavClasses}>
-		<ul>
-			{navLinks.map((navLink, index) => {
-				const { path, title } = navLink;
-				return <li key={index}>
-					<Link href={path}>
-						<a className={router.pathname == path ? styles.active : ""}>
-							{title}
-						</a>
-					</Link>
-				</li>
-			})}
-		</ul>
-	</nav>
-}
+ const router = useRouter();
+ let mobileNavClasses = cx({
+  mobileNav: true,
+ });
+ return (
+  <nav className={mobileNavClasses}>
+   <ul>
+    {navLinks.map((navLink, index) => {
+     const { path, title } = navLink;
+     return (
+      <li key={index}>
+       <Link href={path}>
+        <a className={router.pathname == path ? styles.active : ""}>{title}</a>
+       </Link>
+      </li>
+     );
+    })}
+   </ul>
+  </nav>
+ );
+};
 Nav.Mobile = Mobile;
 export default Nav;
