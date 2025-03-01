b0c10ee69bb1f0f62a2c3fe7d783faa6dcf70dc8
Jeff Passetti <jpassetti@gmail.com>
2025-03-01 12:18:10 -0500

    homepage refactor

--- components/showcase.js
+++ components/showcase.js
@@ -1,28 +1,29 @@
-import styles from './showcase.module.scss'
-import Image from 'next/image'
+import styles from "./showcase.module.scss";
+import Image from "next/image";
 
-const Showcase = ({children}) => {
-	return <div 
-		className={styles.showcase}
-		style={{
-			backgroundImage: `url('/syracuse-university-brass-ensemble-2023.jpg')`
-		}}
-	>	
-		<div className={styles.mobileShowcaseImage}>
-			<Image 
-				src="/syracuse-university-brass-ensemble-2023.jpg"
-				alt="Syracuse University Brass Ensemble"
-				width={1827}
-				height={1306}
-				layout="responsive"
-			/>
-		</div>
-		<div className={styles.content}>
-			<div className={styles.line}></div>
-			The Syracuse University Brass Ensemble (SUBE) is a group of 35 professional-level brass and percussion musicians; members include Syracuse University faculty, staff and students, the State University of New York Upstate Medical University faculty and staff, and accomplished musicians from Upstate New York communities.
-			
-		</div>
+const Showcase = ({ children }) => {
+ return (
+  <div className={styles.showcase}>
+   <Image
+    src="/syracuse-university-brass-ensemble-2023.jpg"
+    alt="Syracuse University Brass Ensemble performing live"
+    layout="fill"
+    objectFit="cover"
+    priority
+   />
+   <div className={styles.overlay}></div>
+   <div className={styles.content}>
+    <div className={styles.line}></div>
+    <p>
+     The Syracuse University Brass Ensemble (SUBE) is a group of 35
+     professional-level brass and percussion musicians; members include Syracuse
+     University faculty, staff, and students, the State University of New York
+     Upstate Medical University faculty and staff, and accomplished musicians
+     from Upstate New York communities.
+    </p>
+   </div>
+  </div>
+ );
+};
 
-	</div>
-}
 export default Showcase;
